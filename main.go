2786261fdf0dbd198ca9a9d64dac1938de2f1cf7
jezzipan <jessica.leute@gmail.com>
2020-12-09 23:29:25 -0300

    tipos, variaveis e valores

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func main() {
-	println("Hello world!")
-}
-
-func Addition(a int, b int) int {
-	return a + b
-}
---
+++ tipos.go
@@ -0,0 +1,15 @@
+//tipos, valores e variaves
+package main // main* pacote principal 
+import (
+	"fmt"
+)
+func main() {
+	//aqui começa
+	x := 16
+	y := "string"
+	z := true
+
+	// hello world
+	// _, erros := fmt.Println("Hello world!","Eaiii", 100)
+	// fmt.Println(erros) //pacote e identificador
+}
