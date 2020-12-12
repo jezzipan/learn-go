fc447b8020ef4d775ccbce6c40c575d415ec9ecd
jezzipan <jessica.leute@gmail.com>
2020-12-11 23:38:09 -0300

    conceitos sobre variaveis

--- tipos.go
+++ tipos.go
@@ -4,12 +4,5 @@ import (
 	"fmt"
 )
 func main() {
-	//aqui começa
-	x := 16
-	y := "string"
-	z := true
-
-	// hello world
-	// _, erros := fmt.Println("Hello world!","Eaiii", 100)
-	// fmt.Println(erros) //pacote e identificador
+	fmt.Println("")
 }
